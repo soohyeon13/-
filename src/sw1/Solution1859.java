4172a747de887c38472d8a2f1bbfd8d6363ba581
soohyeon13 <rlatngus6663@gmail.com>
2019-07-31 03:17:33 +0900

    sw

--- src/sw1/Solution1859.java
+++ src/sw1/Solution1859.java
@@ -1,39 +1,43 @@
 package sw1;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Solution1859 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int testCase = scanner.nextInt();
-        Queue<Integer> list = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ArrayList<Integer> list = new ArrayList<>();
+        StringTokenizer st;
 
-        int[] numList;
+        int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < testCase; i++) {
-            int index = scanner.nextInt();
-            int sum =0;
-            numList = new int[index];
-            for (int j = 0; j < numList.length; j++) {
-                int num = scanner.nextInt();
-                numList[j] = num;
+        for (int i = 1; i <= T; i++) {
+            int num = Integer.parseInt(br.readLine());
+            int sum = 0;
+//            int[] a = new int[num];
+            st = new StringTokenizer(br.readLine(), " ");
+//            int index= a.length;
+            for (int j = 0; j <num ; j++) {
+                list.add(Integer.parseInt(st.nextToken()));
             }
-            list.offer(numList[numList.length-1]);
 
-            for (int j = numList.length-2; j >= 0; j--) {
-                if (list.peek() > numList[j]) {
-                    list.offer(numList[j]);
-                    int a =list.peek();
-                    sum += (a-numList[j]);
-                }else {
-                    list.clear();
-                    list.offer(numList[j]);
+//            list = Arrays.stream(br.readLine().split(" "))
+//                    .map(Integer::parseInt).collect(Collectors.toList());
+            int b = list.get(num-1);
+            System.out.println(b);
+            for (int j = num- 2; j >= 0; j--) {
+                if (b > list.get(j)) {
+                    sum += b - list.get(j);
+                } else {
+                    b = list.get(j);
                 }
             }
-            System.out.println("#"+(i+1) + " " + sum);
             list.clear();
+            bw.write("#" + i + " " + sum + "\n");
         }
+        bw.close();
+        br.close();
     }
 }
