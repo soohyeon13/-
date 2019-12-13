6050daf5ed7a719b83958f7ce09fc29baf8f88db
soohyeon13 <rlatngus6663@gmail.com>
2019-12-13 18:34:17 +0900

    codewars

---
+++ src/codewars/JosephusSurvivor.java
@@ -0,0 +1,9 @@
+package codewars;
+
+//이해 팔요
+public class JosephusSurvivor {
+    public static int josephusSurvivor(final int n,final int k) {
+        if(n == 1) return 1;
+        return (josephusSurvivor(n-1,k)+k-1)%n+1;
+    }
+}
