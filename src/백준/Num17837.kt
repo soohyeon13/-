5e41916e2c867a8590951940cb9fa0be40fd7997
soohyeon13 <rlatngus6663@gmail.com>
2020-05-22 14:24:22 +0900

    섬머코딩 코테 준비(프로그래머스)

--- src/백준/Num17837.kt
+++ src/백준/Num17837.kt
@@ -1,62 +1,179 @@
 package 백준
 
 import java.util.*
+import kotlin.collections.ArrayList
 
-private lateinit var map: Array<Array<String>>
-private lateinit var chessArr : Array<Array<LinkedList<Chess>>>
-private val dx = intArrayOf(0,0,-1,1)
-private val dy = intArrayOf(1,-1,0,0)
+private lateinit var chessArr: Array<Array<LinkedList<Chess>>>
+private lateinit var chessMap: Array<IntArray>
+private lateinit var chessSequence: Array<Chess>
+private var check = false
+private val dx = intArrayOf(0, 0, -1, 1)
+private val dy = intArrayOf(1, -1, 0, 0)
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
     val (n, k) = br.readLine().split(" ").map { it.toInt() }
-    map = Array(n + 1) { Array(n + 1) { "" } }
-    for (i in 1 .. n) {
-        val row = br.readLine().split(" ")
-        for (j in 1.. row.size) {
-            map[i][j] = row[j-1]
+    chessMap = Array(n + 2) { IntArray(n + 2) { 2 } }
+    chessArr = Array(n + 2) { Array(n + 2) { LinkedList<Chess>() } }
+    chessSequence = Array(k + 1) { Chess(0, 0, 0, 0) }
+    for (i in 1..n) {
+        val row = br.readLine().split(" ").map { it.toInt() }
+        for (j in 1..n) {
+            chessMap[i][j] = row[j - 1]
         }
     }
-    chessArr = Array(n+1) { Array(n+1){LinkedList<Chess>() } }
-    val chessQueue : Queue<Chess> = LinkedList()
     for (i in 0 until k) {
-        val (a,b,c) = br.readLine().split(" ").map { it.toInt() }
-        chessArr[a][b].add(Chess(a,b,c))
-        chessQueue.offer(Chess(a,b,c))
+        val (a, b, c) = br.readLine().split(" ").map { it.toInt() }
+        chessArr[a][b].offer(Chess(a, b, i + 1, c))
+        chessSequence[i + 1] = (Chess(a, b, i + 1, c))
     }
-    while (true) {
-        val q = chessQueue.poll()
-        val nx = q.x + dx[q.dir-1]
-        val ny = q.y + dy[q.dir-1]
-        //범위 벗어났을때
-        if (nx < 1 || ny < 1 || nx >n || ny > n) {
-            val d =
-                    if(q.dir == 2) 1 else if (q.dir ==1 ) 2 else if (q.dir == 3) 4 else 3
-            chessArr[nx][ny].remove(Chess(q.x,q.y,q.dir))
-            chessArr[nx][ny].add(Chess(q.x,q.y,d))
-            chessQueue.offer(Chess(q.x,q.y,d))
+    var answer = 1
+    var index = 0
+    loop@ while (true) {
+        val q = chessSequence[index % 4 + 1]
+        val nx = q.x + dx[q.dir - 1]
+        val ny = q.y + dy[q.dir - 1]
+        when (chessMap[nx][ny]) {
+            0 -> {
+                white(q, nx, ny)
+                if (chessArr[nx][ny].size >= 4) break@loop
+            }
+            1 -> {
+                red(q, nx, ny)
+                if (chessArr[nx][ny].size >= 4) break@loop
+            }
+            2 -> {
+                blue(q)
+                if (check) break@loop
+            }
         }
-        //파란색
-        if (map[nx][ny] == "2") {
-            val d =
-                    if(q.dir == 2) 1 else if (q.dir ==1 ) 2 else if (q.dir == 3) 4 else 3
-            val nnx = nx + dx[d]
-            val nny = ny + dy[d]
-            if (map[nnx][nny] == "2") {
-                chessArr[q.x][q.y].remove(Chess(q.x,q.y,q.dir))
-                chessArr[q.x][q.y].add(Chess(q.x,q.y,d))
-                chessQueue.offer(Chess(q.x,q.y,d))
-            }else {
-                
-                chessArr[nnx][nny].add(Chess(q.x,q.y,d))
-                chessQueue.offer(Chess(nnx,nny,d))
+        if (q.position == 1) {
+            if (answer > 1000) {
+                answer = -1
+                break@loop
+            }
+            answer += 1
+        }
+        index += 1
+    }
+    println(answer)
+    bw.flush()
+}
+
+private fun blue(chess: Chess) {
+    check = false
+    var size = chessArr[chess.x][chess.y].size
+    val direction =
+            if (chess.dir == 1) 2 else if (chess.dir == 2) 1 else if (chess.dir == 3) 4 else 3
+    val nnx = chess.x + dx[direction - 1]
+    val nny = chess.y + dy[direction - 1]
+    if (chessMap[nnx][nny] == 2) {
+        for (i in 0 until size) {
+            val c = chessArr[chess.x][chess.y].removeFirst()
+            if (c.position == chess.position) {
+                chessSequence[c.position] = Chess(chess.x, chess.y, c.position, direction)
+                chessArr[chess.x][chess.y].addLast(Chess(chess.x, chess.y, c.position, direction))
+            } else {
+                chessArr[chess.x][chess.y].addLast(c)
+            }
+        }
+    } else {
+        for (j in 0 until size) {
+            val c = chessArr[chess.x][chess.y].removeFirst()
+            if (c.position == chess.position) {
+                chessArr[nnx][nny].addLast(Chess(nnx, nny, c.position, direction))
+                chessSequence[c.position] = Chess(nnx, nny, c.position, direction)
+                while (!chessArr[chess.x][chess.y].isEmpty()) {
+                    val nc = chessArr[chess.x][chess.y].removeFirst()
+                    chessArr[nnx][nny].addLast(Chess(nnx, nny, nc.position, nc.dir))
+                    chessSequence[c.position] = Chess(nnx, nny, nc.position, nc.dir)
+                }
+                break
+            } else {
+                chessArr[chess.x][chess.y].addLast(c)
+            }
+        }
+        if (chessArr[nnx][nny].size >= 4) check = true
+    }
+}
+
+private fun red(chess: Chess, nx: Int, ny: Int) {
+    var size = chessArr[chess.x][chess.y].size
+    val c = chessArr[chess.x][chess.y].removeFirst()
+    if (c.position == chess.position) {
+        if (size == 1) {
+            chessArr[nx][ny].addLast(Chess(nx, ny, c.position, c.dir))
+            chessSequence[c.position] = Chess(nx, ny, c.position, c.dir)
+        } else {
+            while (!chessArr[chess.x][chess.y].isEmpty()) {
+                val nc = chessArr[chess.x][chess.y].removeLast()
+                chessArr[nx][ny].addLast(Chess(nx, ny, nc.position, nc.dir))
+                chessSequence[nc.position] = Chess(nx, ny, nc.position, nc.dir)
+            }
+            chessSequence[c.position] = Chess(nx, ny, c.position, c.dir)
+            chessArr[nx][ny].addLast(Chess(nx, ny, c.position, c.dir))
+        }
+    } else {
+        chessArr[chess.x][chess.y].addLast(c)
+        size -= 1
+        while (size > 0) {
+            val nc = chessArr[chess.x][chess.y].removeFirst()
+            if (nc.position == c.position) {
+                size -= 1
+                for (j in 0 until size) {
+                    val nnc = chessArr[chess.x][chess.y].removeFirst()
+                    chessArr[nx][ny].addLast(Chess(nx, ny, nnc.position, nnc.dir))
+                    chessSequence[nnc.position] = Chess(nx, ny, nnc.position, nnc.dir)
+                }
+                chessArr[nx][ny].addLast(Chess(nx, ny, nc.position, nc.dir))
+                chessSequence[nc.position] = Chess(nx, ny, nc.position, nc.dir)
+                break
+            } else {
+                chessArr[chess.x][chess.y].addLast(nc)
+            }
+            size -= 1
+        }
+    }
+}
+
+private fun white(chess: Chess, nx: Int, ny: Int) {
+    var size = chessArr[chess.x][chess.y].size
+    val c = chessArr[chess.x][chess.y].removeFirst()
+    if (c.position == chess.position) {
+        chessArr[nx][ny].addLast(Chess(nx, ny, c.position, c.dir))
+        chessSequence[chess.position] = (Chess(nx, ny, c.position, c.dir))
+        size -= 1
+        for (i in 0 until size) {
+            val ch = chessArr[chess.x][chess.y].removeFirst()
+            chessSequence[ch.position] = Chess(nx, ny, ch.position, ch.dir)
+            chessArr[nx][ny].addLast(Chess(nx, ny, ch.position, ch.dir))
+        }
+    } else {
+        chessArr[chess.x][chess.y].addLast(c)
+        size -= 1
+        while (size > 0) {
+            val ch = chessArr[chess.x][chess.y].removeFirst()
+            if (ch.position == chess.position) {
+                chessArr[nx][ny].addLast(Chess(nx, ny, ch.position, ch.dir))
+                chessSequence[chess.position] = Chess(nx, ny, ch.position, ch.dir)
+                size -= 1
+                if (size != 0) {
+                    for (i in 0 until size) {
+                        val cch = chessArr[chess.x][chess.y].removeFirst()
+                        chessSequence[cch.position] = Chess(nx, ny, cch.position, cch.dir)
+                        chessArr[nx][ny].addLast(Chess(nx, ny, cch.position, cch.dir))
+                    }
+                }
+            } else {
+                chessArr[chess.x][chess.y].addLast(ch)
             }
         }
     }
 }
 
 private data class Chess(
-        val x : Int,
-        val y : Int,
-        val dir : Int
+        val x: Int,
+        val y: Int,
+        val position: Int,
+        val dir: Int
 )
--- src/윈터코딩2018/LandMove.kt
+++ src/윈터코딩2018/LandMove.kt
@@ -1,36 +1,104 @@
 package 윈터코딩2018
 
 import java.util.*
+import kotlin.Comparator
+import kotlin.collections.ArrayList
 
-private val dx = intArrayOf(-1,0,1,0)
-private val dy = intArrayOf(0,1,0,-1)
+private val dx = intArrayOf(-1, 0, 1, 0)
+private val dy = intArrayOf(0, 1, 0, -1)
+private lateinit var area: Array<IntArray>
+private lateinit var visited: Array<BooleanArray>
+private lateinit var parent : Array<Int>
+private val list = ArrayList<Area>()
+private var index = -1
 class LandMove {
     fun solution(land: Array<IntArray>, height: Int): Int {
-        val dist = Array(land.size){IntArray(land.size){10000}}
-        val queue : Queue<Land> = LinkedList()
-        queue.offer(Land(0,0))
-        dist[0][0] = 0
-        while (!queue.isEmpty()) {
-            val q = queue.poll()
-            for (i in 0 until 4) {
-                val nx = q.x + dx[i]
-                val ny = q.y + dy[i]
-                if (nx < 0 || ny < 0 || nx >= land.size || ny >= land.size) continue
-                if (dist[nx][ny] > Math.abs(land[q.x][q.y]-land[nx][ny])) {
-                    if (Math.abs(land[q.x][q.y]-land[nx][ny]) <= height) {
-                        dist[nx][ny] = 0
-                    }else {
-                        dist[nx][ny] = Math.abs(land[q.x][q.y]-land[nx][ny])
-                    }
-                    queue.offer(Land(nx,ny))
+        var answer = 0
+        visited = Array(land.size) { BooleanArray(land[0].size) }
+        area = Array(land.size) { IntArray(land[0].size) }
+        for (i in land.indices) {
+            for (j in land.indices) {
+                if (!visited[i][j]) {
+                    index +=1
+                    bfs(i,j,land,height)
                 }
             }
         }
-        dist.forEach { println(it.contentToString()) }
-        return 1
+        parent = Array(index+1) { i -> i}
+        for (i in area.indices) {
+            for (j in area.indices) {
+                for (k in 0 until 4) {
+                    val nx = i + dx[k]
+                    val ny = j + dy[k]
+                    if (nx < 0 || ny<0 || nx >= area.size || ny >= area[0].size) continue
+                    if (area[i][j] != area[nx][ny]) {
+                        val diff = Math.abs(land[i][j] - land[nx][ny])
+                        list.add(Area(area[i][j], area[nx][ny],diff))
+                    }
+                }
+            }
+        }
+        val sort =list.sortedWith(Comparator<Area>{p0,p1 ->
+            if (p0.diff < p1.diff) -1
+            else if (p0.diff == p1.diff) {
+                if (p0.first < p1.first) -1
+                else 1
+            }else 1
+        })
+        println(list.toString())
+        var cnt = 0
+        for (element in sort) {
+            if (findParent(element.first) != findParent(element.second)) {
+                union(element.first,element.second)
+                answer += element.diff
+                if (++cnt == index) break
+            }
+        }
+        return answer
     }
-    data class Land(
-            val x : Int,
-            val y : Int
-    )
+}
+private fun findParent(value : Int) : Int {
+    if (value == parent[value]) return value
+    return findParent(parent[value])
+}
+private fun union(first : Int, second : Int) {
+    val f = findParent(first)
+    val s = findParent(second)
+    if (f < s) parent[s] = f
+    else parent[f] = s
+}
+private fun bfs(x: Int, y: Int, land: Array<IntArray>, height: Int) {
+    val queue: Queue<Land> = LinkedList()
+    queue.offer(Land(x, y))
+    visited[x][y] = true
+    area[x][y] = index
+    while (!queue.isEmpty()) {
+        val q = queue.poll()
+        for (i in 0 until 4) {
+            val nx = q.x + dx[i]
+            val ny = q.y + dy[i]
+            if (nx < 0 || ny < 0 || nx >= land.size || ny >= land[0].size) continue
+            if (Math.abs(land[q.x][q.y] - land[nx][ny]) > height) continue
+            if (visited[nx][ny]) continue
+            queue.offer(Land(nx, ny))
+            visited[nx][ny] = true
+            area[nx][ny] = index
+        }
+    }
+}
+
+private data class Area(
+        val first : Int,
+        val second : Int,
+        val diff : Int
+)
+private data class Land(
+        val x: Int,
+        val y: Int
+)
+fun main() {
+    val sol = LandMove()
+    val a = arrayOf(intArrayOf(10, 11, 10, 11),intArrayOf(2, 21, 20, 10),
+            intArrayOf(1, 20, 21, 11),intArrayOf(2, 1, 2, 1))
+    println(sol.solution(a,1))
 }
