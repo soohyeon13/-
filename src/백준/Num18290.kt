e6a4201fca3ffe3f9733a0f0b56ce9cb95d1b67d
soohyeon13 <rlatngus6663@gmail.com>
2020-10-06 18:41:48 +0900

    백준 1189 - dfs(다른 풀이)

--- src/백준/Num18290.kt
+++ src/백준/Num18290.kt
@@ -1,7 +1,5 @@
 package 백준
 
-import kotlin.math.abs
-
 private lateinit var numberMap: Array<IntArray>
 private lateinit var indexMap: IntArray
 private var maxValue = Int.MIN_VALUE
@@ -9,6 +7,7 @@ private val dx = intArrayOf(-1, 0, 1, 0)
 private val dy = intArrayOf(0, 1, 0, -1)
 private var n = 0
 private var m = 0
+
 fun main() {
     val input = System.`in`.bufferedReader()
     val (N, M, K) = input.readLine().split(" ").map { it.toInt() }
@@ -29,35 +28,34 @@ fun main() {
 private fun combination(visited: BooleanArray, start: Int, r: Int) {
     if (r == 0) {
         val arr = ArrayList<Int>()
-        val visitedMap = Array(10) { BooleanArray(10) }
-        for (i in visited.indices) {
-            if (visited[i]) arr.add(i)
-        }
-        if (isCheckStuck(arr,visitedMap)) calculate(arr)
+        visited.forEachIndexed { index, value -> if (value) arr.add(index) }
+        calculate(arr)
         return
     }
-    for (i in start until indexMap.size) {
+    loop@ for (i in start until indexMap.size) {
         visited[i] = true
+        if (!isCheckStuck(visited, i)) {
+            visited[i] = false
+            continue@loop
+        }
         combination(visited, i + 1, r - 1)
         visited[i] = false
     }
 }
 
-private fun isCheckStuck(arr: ArrayList<Int>,visited:Array<BooleanArray>): Boolean {
-    for (i in arr.indices) {
-        val x = arr[i] / m
-        val y = arr[i] % m
-        for (j in 0 until 4) {
-            val nx = x + dx[j]
-            val ny = y + dy[j]
-            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue
-            if (visited[nx][ny]) return false
-            visited[x][y] = true
-        }
+private fun isCheckStuck(visited: BooleanArray, index: Int): Boolean {
+    val x = index / m
+    val y = index % m
+    for (i in 0 until 4) {
+        val nx = x + dx[i]
+        val ny = y + dy[i]
+        if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue
+        val n = nx * m + ny
+        if (visited[n])
+            return false
     }
     return true
 }
-
 
 private fun calculate(arr: ArrayList<Int>) {
     var count = 0
